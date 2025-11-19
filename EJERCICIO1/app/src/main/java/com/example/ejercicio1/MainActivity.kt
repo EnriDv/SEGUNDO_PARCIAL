75672b9840819621d8923d1577107cda6f9f10c5
Enrique Diaz <diazenrive@gmail.com>
2025-11-19 16:08:08 -0400

    FEAT: Agregar los permisos de hardware (ubicacion) al manifest

--- EJERCICIO1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ EJERCICIO1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ejercicio1.ui.theme.EJERCICIO1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EJERCICIO1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    UserCard(nombre = "Yo", fotoUrl = "url", onFollowClick = {})
                 }
             }
         }
@@ -36,6 +38,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun UserCard (nombre: String, fotoUrl: String, onFollowClick: () -> Unit) {
+    Column ( modifier = Modifier.padding(15.dp)){
+        Row (modifier = Modifier.padding(5.dp).innerPadd) {
+            Text( text = "imagen")
+            Text(text = nombre)
+            Text(text = "Estado")
+        }
+
+        Button(onClick = { /*TODO*/ }) { Text(text = "Seguir") }
+
+    }
 }
 
 @Preview(showBackground = true)
